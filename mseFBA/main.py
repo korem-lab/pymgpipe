f274fead4db10e50b32155a1e72904b7358d384a
Yoli Meydan % Korem <ym2877@login5.hpc>
2022-08-23 10:17:31 -0400

    msefba changes

--- mseFBA/main.py
+++ mseFBA/main.py
@@ -1,4 +1,5 @@
 import os
+from re import M
 
 import pandas as pd
 from gurobipy import *
@@ -13,51 +14,50 @@ from pymgpipe.optlang_util import _get_exchange_reactions, _get_reverse_id
 
 import numpy as np
 import sys
+from pathlib import Path
 
 def run(
-    metabolomics_file,
+    metabolomics,
     files=[],
     file_dir='problems/',
     ex_only=True,
     zero_unmapped_metabolites=False,
     threads=int(os.cpu_count()/2),
-    out_file=None,
     solver='gurobi',
     verbosity=0,
     presolve=True,
     threshold=1e-5,
     scale=True,
-    map_labels=True
+    map_labels=True,
+    out_dir='mse/'
 ):
     gc.enable()
 
+    Path(out_dir).mkdir(exist_ok=True)
+        
     try:
         model_files = files if files else [file_dir+m for m in os.listdir(file_dir)]
     except:
         raise Exception('Please pass in a valid model directory using the \'dir\' parameter or an explicit set of files using the \'files\' parameter')
 
-    if not os.path.exists(metabolomics_file):
-        raise Exception('Metabolomics files does not exist.')
+    finished = [f.split('.csv')[0] for f in os.listdir(out_dir)]
+    if len(finished)>0:
+        print('Skipping %s samples that are already finished!'%len(finished))
+        model_files = [f for f in model_files if f.split('/')[-1].split('.mps')[0] not in finished]
 
-    result = pd.DataFrame()
-    threads = os.cpu_count() if threads == -1 else min(threads,len(model_files))
+    threads = os.cpu_count() if threads == -1 else threads
+    threads = min(threads,len(model_files))
 
     print('Solver- %s'%solver.upper())
     print('Presolve- %s'%str(presolve).upper())
     print('Verbosity- %s'%verbosity)
     print('Zero unmapped metabolites- %s'%str(zero_unmapped_metabolites).upper())
     print('Scale metabolomics- %s'%str(scale).upper())
+    print('Map sample IDs- %s'%str(map_labels).upper())
     print('------------------------------------------')
     print('Running mseFBA on %s samples using %s threads...'%(len(model_files),threads))
     
-    if scale:
-        metabolomics_df = scale_metabolomics(metabolomics_file)
-    else:
-        metabolomics_df = pd.read_csv(metabolomics_file,index_col=0)
-
-    if map_labels:
-        conversion = pd.read_csv('sample_label_conversion.csv',index_col=0).conversion.to_dict()
-        metabolomics_df.rename(conversion,axis='columns',inplace=True)
+    metabolomics_df = _get_metabolomics(metabolomics, scale, map_labels)
 
     p = Pool(processes=threads,initializer=partial(_pool_init,metabolomics_df))
     _func = partial(
@@ -67,42 +67,34 @@ def run(
         solver,
         verbosity,
         presolve,
-        threshold
+        threshold,
+        out_dir
     )
-    infeasible_models = []
-    for sample_res in tqdm.tqdm(p.imap(_func, model_files),total=len(model_files)):
-        gc.collect()
+    res = tqdm.tqdm(p.imap(_func, model_files),total=len(model_files))
 
-        if isinstance(sample_res,str):
-            infeasible_models.append(sample_res)
-            continue
+    feasible_models = filter(lambda sample: sample[1] == True, res)
+    infeasible_models = filter(lambda sample: sample[1] == False, res)
 
-        result = pd.concat([result,sample_res],axis=1)
-        result.sort_index(axis=1,inplace=True) # sort columns
-        result.sort_index(axis=0,inplace=True) # sort index
-        if out_file is not None:
-            result.to_csv(out_file)
     p.close()
     p.join()
 
-    print('Finished mseFBA!')
+    print('Finished mseFBA! Solved %s samples'%len(feasible_models))
     if len(infeasible_models) > 0:
-        print('!!! Some models were infeasible and could not be solved-\n')
+        print('Some models were infeasible and could not be solved-\n')
         print(infeasible_models)
 
-    return result
 
-def _mseFBA_worker(ex_only, zero_unmapped_metabolites, solver, verbosity, presolve, threshold, model_file):
+def _mseFBA_worker(ex_only, zero_unmapped_metabolites, solver, verbosity, presolve, threshold, out_dir, model_file):
     model = load_model(model_file,solver)
     
     metabolite_dict = metabolomics_global[model.name].to_dict()
-    _add_correlation_objective(model,metabolite_dict)
-
     if zero_unmapped_metabolites:
         ex_reactions = _get_exchange_reactions(model)
-        unmapped_map = {k:0 for k in ex_reactions if k not in metabolite_dict}
-        _add_correlation_objective(model,unmapped_map)
+        metabolite_dict.update({k:0 for k in ex_reactions if k not in metabolite_dict})
+        
+    _add_correlation_objective(model,metabolite_dict)
 
+    solved = False
     try:
         solution = solve_model(
             model=model,
@@ -112,11 +104,29 @@ def _mseFBA_worker(ex_only, zero_unmapped_metabolites, solver, verbosity, presol
             method='barrier',
             flux_threshold=threshold
         )
-        del model
-        return solution
+        solved=True
     except InfeasibleModelException:
-        del model
-        return model_file
+        pass
+
+    del model
+    gc.collect()
+
+    solution.sort_index(axis=0,inplace=True)
+    solution.to_csv(out_dir+'%s.csv'%model.name)
+    
+    return (model_file,solved)
+
+def _get_metabolomics(metabolomics,scale=True,map_labels=True):
+    metabolomics_df = _load_dataframe(metabolomics)
+    
+    if scale:
+        metabolomics_df = scale_metabolomics(metabolomics_df)
+
+    if map_labels:
+        conversion = _load_dataframe('sample_label_conversion.csv').conversion.to_dict()
+        metabolomics_df.rename(conversion,axis='columns',inplace=True)
+
+    return metabolomics_df
 
 def _pool_init(df):
     sys.stdout = open(os.devnull, 'w')  
@@ -124,23 +134,15 @@ def _pool_init(df):
     global metabolomics_global
     metabolomics_global = df
 
-def scale_metabolomics(metabolomics_path,force=False):
-    print('Scaling metabolomics at path %s...'%metabolomics_path)
+def scale_metabolomics(metabolomics,fva_dir='fva/'):
+    print('Scaling metabolomics...')
 
-    if not os.path.exists(metabolomics_path):
-        raise Exception('Could not find metabolomics file at specified path- %s'%metabolomics_path)
+    raw = _load_dataframe(metabolomics)
 
-    out_path = metabolomics_path if metabolomics_path.endswith('_scaled.csv') else metabolomics_path.split('/')[-1].split('.csv')[0]+'_scaled.csv'
-    if os.path.exists(out_path) and not force:
-        print('Found existing scaled metabolomics, skipping this step. If you want to repeat, delete %s.'%out_path)
-        return pd.read_csv(out_path,index_col=0)
+    if not os.path.exists(fva_dir):
+        raise Exception('Could not find fva directory at %s'%fva_dir)
 
-    raw = pd.read_csv(metabolomics_path,index_col=0)
-
-    if not os.path.exists('fva/'):
-        raise Exception('Could not find fva/ file directory. Please place FVA sample results in folder titled \'fva\'')
-
-    fva_dfs = [pd.read_csv('fva/%s'%f,index_col=0) for f in os.listdir('fva/')]
+    fva_dfs = [_load_dataframe(fva_dir+f) for f in os.listdir(fva_dir)]
     if len(fva_dfs)==0:
         raise Exception('No FVA results found')
 
@@ -158,7 +160,7 @@ def scale_metabolomics(metabolomics_path,force=False):
 
     missing_metabs = [m for m in list(raw.index) if m not in min_max]
     if len(missing_metabs)>0:
-        print('!!! Skipping %s metabolites due to them being missing from models, these metabolites will not be included in formatted metabolomics...\n'%len(missing_metabs)) 
+        print('Skipping %s metabolites due to them being missing from models, these metabolites will not be included in formatted metabolomics...\n'%len(missing_metabs)) 
         raw.drop(missing_metabs,inplace=True)
 
     def _scale_row(x):
@@ -176,7 +178,6 @@ def scale_metabolomics(metabolomics_path,force=False):
     if len(scaled.index) == 0:
         raise Exception('Scaled metabolomics is empty. Please check FVA files to make sure everything is correct!')
 
-    scaled.to_csv(out_path)
     return scaled
 
 def _add_correlation_objective(model, flux_map):
@@ -195,3 +196,13 @@ def _add_correlation_objective(model, flux_map):
     
     model.objective = Objective(obj_expression,direction="min")
     model.update()
+
+def _load_dataframe(m):
+    if isinstance(m,str):
+        if not os.path.exists(m):
+            raise Exception('Tried to load dataframe from path that does not exist- %s'%m)
+        return pd.read_csv(m,index_col=0)
+    elif isinstance(m, pd.DataFrame):
+        return m
+    else:
+        raise Exception('_load_dataframe can only take a string or dataframe, received %s'%type(m))
