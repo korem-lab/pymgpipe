30bb4e305beff6b9120965d594b7229424713b7f
Yoli Meydan <ym2877@cumc.columbia.edu>
2023-02-07 09:17:50 -0500

    Refactor structure

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         "tqdm",
     ],
     url="https://github.com/korem-lab/pymgpipe",
-    package_dir={"pymgpipe": "src/pymgpipe"},
+    package_dir={"pymgpipe": "pymgpipe"},
     packages=["pymgpipe"],
     package_data={
         "pymgpipe": [
