ff31a9a18e6469267258bdf8dd8f8df4b048df82
Yoli Meydan <ym2877@cumc.columbia.edu>
2023-02-15 18:41:44 -0500

    Added test

--- pymgpipe/tests/test_diet.py
+++ pymgpipe/tests/test_diet.py
@@ -22,7 +22,7 @@ def test_remove_diet(mini_cobra_model):
     assert curr_diet.lb.unique()[0] == -1000 and curr_diet.ub.unique()[0] == 1000
 
 
-def test_personlized_diet(mini_cobra_model):
+def test_personlized_diet_force_uptake(mini_cobra_model):
     my_diet = [
         ["EX_pi(e)", 30.96],
         ["EX_h2o(e)", 35.59],
@@ -40,6 +40,34 @@ def test_personlized_diet(mini_cobra_model):
         ["Diet_EX_pyr[d]", -26.32, -21.056],
         ["Diet_EX_gln__L[d]", -44.78, -35.824],
         ["Diet_EX_pi[d]", -30.96, -24.768],
+    ]
+    expected = pd.DataFrame(data=expected_bounds, columns=["id", "lb", "ub"])
+    expected.set_index("id", inplace=True)
+
+    added_diet = get_diet(mini_cobra_model).set_index("id").loc[expected.index]
+
+    remove_diet(mini_cobra_model)
+    assert expected.round(3).equals(added_diet.loc[expected.index].round(3))
+
+
+def test_personalized_diet_no_force_uptake(mini_cobra_model):
+    my_diet = [
+        ["EX_pi(e)", 30.96],
+        ["EX_h2o(e)", 35.59],
+        ["EX_gln__L(e)", 44.78],
+        ["EX_pyr(e)", 26.32],
+    ]
+    diet = pd.DataFrame(data=my_diet, columns=["Reaction", "flux"])
+    diet.set_index(["Reaction"], inplace=True)
+
+    remove_diet(mini_cobra_model)
+    add_diet_to_model(mini_cobra_model, diet, force_uptake=False)
+
+    expected_bounds = [
+        ["Diet_EX_h2o[d]", -35.59, 0],
+        ["Diet_EX_pyr[d]", -26.32, 0],
+        ["Diet_EX_gln__L[d]", -44.78, 0],
+        ["Diet_EX_pi[d]", -30.96, 0],
     ]
     expected = pd.DataFrame(data=expected_bounds, columns=["id", "lb", "ub"])
     expected.set_index("id", inplace=True)
