d897a947ae3fa3de05338572bba0064560c433c2
Yoli Meydan <ym2877@cumc.columbia.edu>
2023-02-27 13:31:37 -0500

    Update build.py

--- pymgpipe/build.py
+++ pymgpipe/build.py
@@ -21,6 +21,18 @@ def build(
     coupling_constraints=True,
     diet_fecal_compartments=True,
 ):
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        arg1 (int): Description of arg1
+        arg2 (str): Description of arg2
+
+    Returns:
+        bool: Description of return value
+
+    """
     if not solver:
         solver = [
             s
