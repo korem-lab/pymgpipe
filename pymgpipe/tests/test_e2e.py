125dda4b2f120f62354acb8d9c2f6a3c9fc020a8
Yoli Meydan <ym2877@cumc.columbia.edu>
2023-02-28 19:29:40 -0500

    Added full e2e

--- pymgpipe/tests/test_e2e.py
+++ pymgpipe/tests/test_e2e.py
@@ -1,13 +1,47 @@
 import optlang
 import os
 import pandas as pd
+import numpy as np
 import cobra
 from pkg_resources import resource_filename
-from pymgpipe import add_coupling_constraints, compute_nmpcs, get_abundances
+from pymgpipe import add_coupling_constraints, compute_nmpcs, get_abundances, build_models
 from pymgpipe.build import _build
 from pytest_check import check
 import re
+import tempfile
 
+
+
+def test_build_models():
+    samples = ['sample%s'%i for i in range(5)]
+    cov = pd.DataFrame(columns=samples,index=['TaxaA','TaxaB','TaxaC','TaxaD'])
+
+    for t in cov.columns:
+        cov[t]=np.random.dirichlet(np.ones(4),size=1)[0]
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        build_models(
+            coverage_file=cov,
+            taxa_dir=resource_filename("pymgpipe", "resources/miniTaxa/"),
+            threads=-1,
+            parallel=True,
+            out_dir=tmpdirname,
+            diet_fecal_compartments=True,
+            coupling_constraints=True,
+            compute_metrics=True,
+            compress=True,
+            write_lp=True
+        )
+        models_out = os.listdir(tmpdirname+'/models/')
+        problems_out = os.listdir(tmpdirname+'/problems/')
+
+        assert len(models_out)==5 and len(problems_out) == 5
+
+        assert \
+            os.path.exists(tmpdirname+'/metabolic_diversity.png') and \
+            os.path.exists(tmpdirname+'/reaction_abundance.csv') and \
+            os.path.exists(tmpdirname+'/reaction_content.csv') and \
+            os.path.exists(tmpdirname+'/sample_label_conversion.csv')
 
 def test_full_diet_fecal_compartments():
     sample_data = [
